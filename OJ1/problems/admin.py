28b5b6d3f1e87b0af4c4b41dfe2d8cd906ff771c
PratyushSingh1805 <singhpratyush1805@gmail.com>
2025-05-18 18:46:23 +0530

    Added problem editing using admin

--- OJ1/problems/admin.py
+++ OJ1/problems/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Problem
+
+#admin.site.register(Problem)
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'difficulty', 'tags')
+    search_fields = ('title', 'tags')
+    list_filter = ('difficulty',)
